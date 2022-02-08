1ab5bebc8043e3d9b58e089928a54523a397d1cd
wblanck <blanck.william@gmail.com>
2022-02-08 15:48:50 +0100

    singlecard Comp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import "./App.css";
+import SingleCard from "./components/SingleCard";
 
 const cardImages = [
-  { src: "/public/img/helmet-1.png" },
-  { src: "/public/img/potion-1.png" },
-  { src: "/public/img/ring-1.png" },
-  { src: "/public/img/scroll-1.png" },
-  { src: "/public/img/shield-1.png" },
-  { src: "/public/img/sword-1.png" },
+  { src: "/img/helmet-1.png" },
+  { src: "/img/potion-1.png" },
+  { src: "/img/ring-1.png" },
+  { src: "/img/scroll-1.png" },
+  { src: "/img/shield-1.png" },
+  { src: "/img/sword-1.png" },
 ];
 
 function App() {
@@ -26,6 +27,11 @@ function App() {
     <div className="App">
       <h1>Magic Match</h1>
       <button onClick={shuffleCards}>New Game</button>
+      <div className="card-grid">
+        {cards.map(({ id, src }) => (
+          <SingleCard key={id} src={src} />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/components/SingleCard.jsx
@@ -0,0 +1,14 @@
+import "./SingleCard.css";
+
+function SingleCard({ src }) {
+  return (
+    <div className="card">
+      <div>
+        <img className="front" src={src} alt="card front" />
+        <img className="back" src="/img/cover.png" alt="card back" />
+      </div>
+    </div>
+  );
+}
+
+export default SingleCard;
